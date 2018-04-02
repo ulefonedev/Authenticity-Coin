0e87214a250a022c7c821d29ba4418297a57e9f1
imtiazmobile <11322940+imtiazmobile@users.noreply.github.com>
2018-04-03 02:29:27 +0800

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -144,8 +144,8 @@ const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const std::initializer_list<const char*> SEED_NODES = {
-   "144.202.35.236:17236",
-   "45.32.171.244:17236"
+   "140.82.29.104:17236",
+   "45.77.160.124:17236"
 };
 
 struct CheckpointData {
