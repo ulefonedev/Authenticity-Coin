25b84b61b787faeb4e1310ce291e68dada0ca9df
imtiazmobile <11322940+imtiazmobile@users.noreply.github.com>
2018-04-07 14:56:39 +0800

    .

--- build/release/external/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/intuCoin/external
-# Build directory: /root/intuCoin/build/release/external
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(miniupnpc)
-subdirs(gtest)
--- build/release/external/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/intuCoin/external/gtest
-# Build directory: /root/intuCoin/build/release/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
